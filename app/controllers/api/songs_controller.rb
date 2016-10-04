568103c61c2bc72ee3c975e871895c30bf5c3ef0
David Thomas & Kathryn Ragudos <pair+kawikathomas.kragudos19@devbootcamp.com>
2016-10-04 16:27:26 -0700

    Push up to ionic for spotify connection attempt.

--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -10,9 +10,10 @@ class Api::SongsController < ApplicationController
       render nothing: true # no error is rendered
     else
       song_params = params[:song]
-      @song = Song.create(title: song_params[:title], artist: song_params[:artist_name])
-      PlaylistSong.create(playlist_id: current_user.playlist.id, song_id: @song.id)
-      redirect_to api_artist_path(song_params[:artist_id])
+      @song ||= Song.create(title: song_params[:title], artist: song_params[:artist_name])
+      @playlist_song = PlaylistSong.create(playlist_id: current_user.playlist.id, song_id: @song.id)
+      render :json, {song: @song, playlist_song: @playlist_song}
+      # redirect_to api_artist_path(song_params[:artist_id])
         # @song = Song.new
       # @song.assign_attributes(title: @json['title'], artist: @json['artist'])
       # render json: @song if @song.save
