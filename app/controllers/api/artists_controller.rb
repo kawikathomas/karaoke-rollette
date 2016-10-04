9ff76d161f22a60f2451f7eaba3dd49a5d1f35d7
David Thomas & Kathryn Ragudos <pair+kawikathomas.kragudos19@devbootcamp.com>
2016-10-04 16:47:11 -0700

    test connection

--- app/controllers/api/artists_controller.rb
+++ app/controllers/api/artists_controller.rb
@@ -1,13 +1,15 @@
 class Api::ArtistsController < ApplicationController
 
  def index
-   if !params[:artist_name].empty?
+    puts "is this working"
+   if params[:artist_name]
      @artists = RSpotify::Artist.search(params[:artist_name])
      render :json, {artists: @artists}
    # else
    #   redirect_to root_path
    end
  end
+
  def show
   @artist = RSpotify::Artist.find(params[:id])
 end
