d90ab2671040cc60f9aa513ab12a5d3f87159530
Greg McGough <gjmcgough@gmail.com>
2016-10-05 13:29:54 -0700

    update this sheeeet

--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -8,7 +8,10 @@ class Api::SongsController < ApplicationController
   end
 
   def create
+      p params
+      p @json['title']
       @song ||= Song.create(title: @json['title'], artist: @json['artist'], image_src: @json['img_src'])
+      puts (@json['title'])
       @playlist ||= Playlist.create(user_id: @user.id)
       @playlist_song = PlaylistSong.create(playlist_id: @playlist.id, song_id: @song.id)
   end
