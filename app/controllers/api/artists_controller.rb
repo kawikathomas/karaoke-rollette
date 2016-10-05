52e144d8dfd9b016985cb193dba05ce077da1291
David Thomas & Kathryn Ragudos <pair+kawikathomas.kragudos19@devbootcamp.com>
2016-10-04 17:45:12 -0700

    Change search to trackS.

--- app/controllers/api/artists_controller.rb
+++ app/controllers/api/artists_controller.rb
@@ -4,12 +4,12 @@ class Api::ArtistsController < ApplicationController
     puts "#{params.inspect} ==========================================="
 
     if params[:track_name]
-       @tracks = RSpotify::Track.search(params[:track_name])
+       @tracks = RSpotify::Tracks.search(params[:track_name])
        render json: @tracks.to_json
     end
   end
 
  def show
-  @track = RSpotify::Track.find(params[:id])
+  @track = RSpotify::Tracks.find(params[:id])
 end
 end
