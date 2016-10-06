45c98f971b21dbed68a4c7469293a84ca4685841
Greg McGough <gjmcgough@gmail.com>
2016-10-05 18:52:53 -0700

    new song match data

--- app/controllers/api/song_matches_controller.rb
+++ app/controllers/api/song_matches_controller.rb
@@ -10,10 +10,10 @@ class Api::SongMatchesController < ApplicationController
   end
 
   def matches_data
-    @user = User.find(@user.id)
-    render json: @user.as_json(include: [:party, :song_matches, playlist: {include: :songs}])
-    # @song_matches = SongMatch.where(party_id: @user.party.id)
-    # render json: @song_matches.as_json(include: [:users, playlists: {include: :songs}])
+    # @user = User.find(@user.id)
+    # render json: @user.as_json(include: [:party, :song_matches, playlist: {include: :songs}])
+    @song_matches = SongMatch.where(party_id: @user.party.id)
+    render json: @song_matches.as_json(include: [:users, :songs, :party])
   end
 
   def destroy
