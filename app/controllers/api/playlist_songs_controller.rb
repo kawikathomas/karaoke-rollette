6df6001cedcfc1d169f344567c4f5978197fd6d8
Kathryn Ragudos <kragudos19@gmail.com>
2016-10-05 18:02:47 -0700

    fix actual routes this time

--- app/controllers/api/playlist_songs_controller.rb
+++ app/controllers/api/playlist_songs_controller.rb
@@ -1,33 +1,9 @@
 class Api::PlaylistSongsController < ApplicationController
-  before_action :set_playlist, only: [:create, :update]
-  before_action :find_playlist_song, only: [:create, :update]
-
-  def new
-    @playlistSong = PlaylistSong.new
-    render json: { playlistsong: @playlistSong }
-  end
-
-  def create
-    if @playlistSong.present?
-      render nothing: true
-      head 208 #already reported status code
-    else
-      @playlistSong = PlaylistSong.create!(playlist_id: @playlist.id, song_id: @json['song_id'])
-      render json: @playlistSong
-    end
-  end
+  before_action :set_user
 
   def destroy
-    @playlistSong = PlaylistSong.find_by(params)
-  end
-
-  private
-  def set_playlist #finds the playlist associated with user
-    @playlist = User.find_by(name: params[:name]).playlist
-  end
-
-  def find_playlist_song
-    @playlistSong = PlaylistSong.find_by(playlist_id: @playlist.id, song_id: @json[:song_id])
+    @playlistsong = PlaylistSong.find_by(song_id: params[:id], playlist_id: @user.playlist.id)
+    @playlistsong.destroy
   end
 
 end
