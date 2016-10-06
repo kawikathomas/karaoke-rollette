763aa6653a12a8317a6f3a5a02d3e4418114b7e6
Greg McGough <gjmcgough@gmail.com>
2016-10-06 12:21:35 -0700

    cmon trav

--- app/controllers/games_controller.rb
+++
@@ -1,8 +0,0 @@
-class GamesController < ApplicationController
-
-
-
-end
-
-
-
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,4 @@
 class SessionsController < ApplicationController
-  def show
-
-  end
-
-  # def create
-  #   User.from_omniauth(env["omniauth.auth"])
-  #   session[:user_id] = user.id
-  # end
 
   def destroy
     session[:user_id] = nil
