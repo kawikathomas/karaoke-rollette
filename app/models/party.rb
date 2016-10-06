8595f538d539f0ba237e24b31383155217d41e7c
Greg McGough <gjmcgough@gmail.com>
2016-10-06 11:14:05 -0700

    clean up

--- app/models/party.rb
+++ app/models/party.rb
@@ -8,7 +8,6 @@ class Party < ApplicationRecord
     Array.new(5) { charset.sample }.join
   end
 
- # build party queue
   def build_playlist
     party_queue = []
     queues = playlists.each do |playlist|
@@ -18,19 +17,5 @@ class Party < ApplicationRecord
     end
     party_queue
   end
-
-  # def song
-  #   party_playlist = @party.build_playlist
-  #   index = rand(0..party_playlist.length)
-  #   @song = party_playlist[index]
-  #   render @song.to_json
-  # end
-
-  # def singer
-  #   game_users = @party.users
-  #   index = rand(0..game_users.length)
-  #   @singer = game_users[index]
-  #   render @singer.to_json
-  # end
 
 end
