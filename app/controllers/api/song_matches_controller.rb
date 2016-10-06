ade51ab07f457807cd034ebc9ec995b6eeacbec7
Greg McGough <gjmcgough@gmail.com>
2016-10-05 18:33:28 -0700

    spellchack

--- app/controllers/api/song_matches_controller.rb
+++ app/controllers/api/song_matches_controller.rb
@@ -4,18 +4,19 @@ class Api::SongMatchesController < ApplicationController
   before_action :random_singer_id
 
   def create
-    @song_match = SongMatch.new(user_id: random_singer_id, song_id: random_song_id, party_id: @party.id)
+    @song_match = SongMatch.new(user_id: random_singer_id, song_id: random_song_id, party_id: @user.party.id)
     @song_match.save
     render @song_match.to_json
   end
 
   def matches_data
     @song_matches = SongMatch.where(party_id: @user.party.id)
+    puts "#{@song_matches}=============================================================="
     render json: @song_matches.as_json
   end
 
   def destroy
-    @song_match = SongMatch.find_by(user_id: random_singer_id, song_id: random_song_id, party_id: @party.id)
+    @song_match = SongMatch.find_by(user_id: random_singer_id, song_id: random_song_id, party_id: @user.party.id)
     @song_match.destroy
   end
 
