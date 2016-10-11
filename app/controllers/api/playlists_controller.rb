4e0b2a7ec20ada13194798c2cefeb8375268fee6
David Thomas <dthomas865@gmail.com>
2016-10-11 16:02:16 -0700

    clean out unneccessary clone to increase readability

--- app/controllers/api/playlists_controller.rb
+++ app/controllers/api/playlists_controller.rb
@@ -1,14 +1,8 @@
-class Api::PlaylistsController < ApplicationController
-  # before_action :set_playlist
+class Api::PlaylistsController < ApplicationControlle
   before_action :set_user
 
   def index
-    # playlist = @user.playlist.playlist_songs.order('created_at DESC')
     render json: @user.playlist.to_json(include: [:songs, :user])
-  end
-
-  def destroy
-    # User cannot destroy their playlist, they can only delete songs and empty playlist
   end
 
 
--- app/models/party.rb
+++ app/models/party.rb
@@ -18,19 +18,5 @@ class Party < ApplicationRecord
     end
     party_queue
   end
-
-  # def song
-  #   party_playlist = @party.build_playlist
-  #   index = rand(0..party_playlist.length)
-  #   @song = party_playlist[index]
-  #   render @song.to_json
-  # end
-
-  # def singer
-  #   game_users = @party.users
-  #   index = rand(0..game_users.length)
-  #   @singer = game_users[index]
-  #   render @singer.to_json
-  # end
-
+  
 end
